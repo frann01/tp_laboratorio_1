e5a1ea59e525a897e5d8c1cccfaed3d56c264f11
frann01 <67615886+frann01@users.noreply.github.com>
2021-06-18 16:10:16 -0300

    Commit tp 3 parser.h

---
+++ tp3_Szellner/parser.h
@@ -0,0 +1,17 @@
+/*
+ * parser.h
+ *
+ *  Created on: 18 jun. 2021
+ *      Author: USUARIO
+ */
+
+#ifndef PARSER_H_
+#define PARSER_H_
+
+
+
+#endif /* PARSER_H_ */
+
+int parser_EmployeeFromBinary(FILE* pFile, LinkedList* pArrayListEmployee);
+
+int parser_EmployeeFromText(FILE* pFile, LinkedList* pArrayListEmployee);
